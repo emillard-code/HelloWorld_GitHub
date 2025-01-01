5dbc21bee61e60bb819d838eab62a0a89aafef37
Emerson Millard <emillard.code@gmail.com>
2025-01-01 12:00:00 -0800

    Added simple JUnit print testing.

--- src/test/java/com/project/MainTest.java
+++ src/test/java/com/project/MainTest.java
@@ -1,5 +1,27 @@
 package com.project;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class MainTest {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void helloWorldTest() {
+
+        System.out.println("Hello World");
+        Assertions.assertEquals("Hello World", outputStreamCaptor.toString().trim());
+
+    }
 
 }
